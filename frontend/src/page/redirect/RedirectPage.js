204a0beab76e16d40ef16a67878ae33b8f5e6ac8
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-12-25 23:22:11 +0900

    feat(RedirectPage): 리다이렉트 구현

--- frontend/src/page/redirect/RedirectPage.js
+++ frontend/src/page/redirect/RedirectPage.js
@@ -1,13 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from 'axios';
 
 const RedirectPage = () => {
 
 const { shortUrl } = useParams();
+const navigate = useNavigate();
 
-    if(shortUrl) 
-    // window.location.href = 'https://www.google.com';
-
-  return null;
+async function getShortUrl() {
+    await axios.get(`http://localhost:8080/shorts/${shortUrl}`)
+    .then((res) => {
+        console.log(res.data.full);
+        window.location.href = res.data.full;
+        return ;
+    })
+    .catch((e) => {
+        console.log("일치하는 url 없음", e);
+        alert('일치하는 url이 없습니다.');
+        return navigate('/');
+    })
+}
+    getShortUrl();
 }
 
-export default RedirectPage
+export default RedirectPage;
