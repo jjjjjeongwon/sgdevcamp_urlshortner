468cbf9113e2f6879b9802735d4a20acd5d1c710
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-12-26 02:47:03 +0900

    style(*): prettier 적용

--- frontend/src/page/redirect/RedirectPage.js
+++ frontend/src/page/redirect/RedirectPage.js
@@ -1,25 +1,25 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const RedirectPage = () => {
+  const { shortUrl } = useParams();
+  const navigate = useNavigate();
 
-const { shortUrl } = useParams();
-const navigate = useNavigate();
-
-async function getShortUrl() {
-    await axios.get(`http://localhost:8080/shorts/${shortUrl}`)
-    .then((res) => {
+  async function getShortUrl() {
+    await axios
+      .get(`http://localhost:8080/shorts/${shortUrl}`)
+      .then((res) => {
         console.log(res.data.full);
         window.location.href = res.data.full;
-        return ;
-    })
-    .catch((e) => {
-        console.log("일치하는 url 없음", e);
+        return;
+      })
+      .catch((e) => {
+        console.log('일치하는 url 없음', e);
         alert('일치하는 url이 없습니다.');
         return navigate('/');
-    })
-}
-    getShortUrl();
-}
+      });
+  }
+  getShortUrl();
+};
 
 export default RedirectPage;
